df221086b88014094a44dc3c0ec074c04e3b7f21
dam-di <cfgs.ddi@gmail.com>
2022-04-10 01:14:36 +0200

    Nuevos archivos

--- GRUA/ControlGrua.py
+++ GRUA/ControlGrua.py
@@ -45,8 +45,8 @@ class ControlGrua(object):
 
     def tareaMoverCarro(self, direccion):
         global carroOn
-        #button_state = GPIO.input(18)
-        while (carroON or GPIO.input(18)):
-            print()
-            #button_state = GPIO.input(18)
+        button_state = GPIO.input(18)
+        while (carroON or button_state):
+
+            button_state = GPIO.input(18)
             self.amspi.run_dc_motor(self.amspi.DC_Motor_3, clockwise=direccion)
