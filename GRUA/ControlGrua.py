eb83db7256b6c963430e30eeb3063220ae25f5ba
dam-di <cfgs.ddi@gmail.com>
2022-04-10 00:53:24 +0200

    Nuevos archivos

--- GRUA/ControlGrua.py
+++ GRUA/ControlGrua.py
@@ -3,13 +3,16 @@
 from AMSpi import AMSpi
 from threading import Thread
 from time import sleep
+import RPi.GPIO as GPIO
+
 global carroON
 
 class ControlGrua(object):
     def __init__(self):
         self.amspi = AMSpi()
         global carroON
-
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup (18, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         # Set PINs for controlling shift register (GPIO numbering)
         self.amspi.set_74HC595_pins(21, 20, 16)
         # Set PINs for controlling all 4 motors (GPIO numbering)
@@ -42,5 +45,5 @@ class ControlGrua(object):
 
     def tareaMoverCarro(self, direccion):
         global carroOn
-        while (carroON):
+        while (carroON or GPIO.input(18)):
             self.amspi.run_dc_motor(self.amspi.DC_Motor_3, clockwise=direccion)
