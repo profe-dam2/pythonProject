e24728f4ef93d7a0c9ea382eef89d8fe22c2463c
dam-di <cfgs.ddi@gmail.com>
2022-04-10 13:08:09 +0200

    Nuevos archivos

--- GRUA/ControlGrua.py
+++ GRUA/ControlGrua.py
@@ -19,7 +19,7 @@ class ControlGrua(object):
         self.amspi.set_74HC595_pins(21, 20, 16)
         # Set PINs for controlling all 4 motors (GPIO numbering)
         self.amspi.set_L293D_pins(5, 6, 13, 19)
-
+        ganchoON = False
         carroON = False
         print('INICIAL CONTROL GRUA')
         #self.prueba()
